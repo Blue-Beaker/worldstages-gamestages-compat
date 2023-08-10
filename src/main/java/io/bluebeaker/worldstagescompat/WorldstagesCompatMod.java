977ed5c641883071bc3e8bf231b9c6b0711368f1
blue-beaker <yyy5566778899@126.com>
2023-08-10 21:31:04 +0800

    stage

--- src/main/java/io/bluebeaker/worldstagescompat/WorldstagesCompatMod.java
+++ src/main/java/io/bluebeaker/worldstagescompat/WorldstagesCompatMod.java
@@ -1,16 +1,17 @@
 package io.bluebeaker.worldstagescompat;
 import net.darkhax.gamestages.event.GameStageEvent;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.eventhandler.Event.Result;
-
 import org.apache.logging.log4j.Logger;
 
-import io.bluebeaker.worldstages.StageChecker;
-import io.bluebeaker.worldstages.WorldStagesConfig;
-import io.bluebeaker.worldstages.WorldStagesWorldSavedData;
+import io.bluebeaker.worldstages.WorldStagesSavedData;
 import io.bluebeaker.worldstages.ConfigStorage;
+import io.bluebeaker.worldstages.WorldStageEvent;
 // The value here should match an entry in the META-INF/mods.toml file
 
 @Mod(modid = WorldstagesCompatMod.MODID, name = WorldstagesCompatMod.NAME, version = WorldstagesCompatMod.VERSION)
@@ -19,27 +20,41 @@ public class WorldstagesCompatMod
     public static final String MODID = "worldstagescompat";
     public static final String NAME = "Worldstages Compat";
     public static final String VERSION = "1.0";
+    
+    public MinecraftServer server;
 
     private static Logger logger;
-
+    
     public WorldstagesCompatMod() {
         MinecraftForge.EVENT_BUS.register(this);
     }
     @SubscribeEvent
     public void onCheckStage(GameStageEvent.Check event){
-        if(WorldStagesWorldSavedData.get(event.getEntityPlayer().world).stages.contains(event.getStageName()))
+        if(WorldStagesSavedData.get(event.getEntityPlayer().world).getStages().contains(event.getStageName()))
         event.setHasStage(true);
     }
     @SubscribeEvent
     public void onAddStage(GameStageEvent.Add event){
         if(ConfigStorage.instance.RegisteredStages.contains(event.getStageName()))
-        WorldStagesWorldSavedData.get(event.getEntityPlayer().world).addStage(event.getStageName());
+        WorldStagesSavedData.get(event.getEntityPlayer().world).addStage(event.getStageName());
         event.setCanceled(true);
     }
     @SubscribeEvent
     public void onRemoveStage(GameStageEvent.Remove event){
         if(ConfigStorage.instance.RegisteredStages.contains(event.getStageName()))
-        WorldStagesWorldSavedData.get(event.getEntityPlayer().world).removeStage(event.getStageName());
+        WorldStagesSavedData.get(event.getEntityPlayer().world).removeStage(event.getStageName());
         event.setCanceled(true);
+    }
+    @EventHandler
+    public void onServerStart(FMLServerStartingEvent event){
+        this.server=event.getServer();
+    }
+    @SubscribeEvent
+    public void onStageChanged(WorldStageEvent event){
+        if(this.server!=null){
+            for(EntityPlayerMP player: server.getPlayerList().getPlayers()){
+                MinecraftForge.EVENT_BUS.post(new GameStageEvent(player, event.laststage));
+            }
+        }
     }
 }
