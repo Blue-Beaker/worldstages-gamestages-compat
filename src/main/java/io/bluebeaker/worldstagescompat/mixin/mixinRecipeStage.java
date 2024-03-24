a181239e29b17b0d3a3e7f947cd575478382c705
blue-beaker <yyy5566778899@126.com>
2024-03-24 17:59:53 +0800

    mixin recipe stage

---
+++ src/main/java/io/bluebeaker/worldstagescompat/mixin/mixinRecipeStage.java
@@ -0,0 +1,23 @@
+package io.bluebeaker.worldstagescompat.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.blamejared.recipestages.recipes.RecipeStage;
+
+import io.bluebeaker.worldstages.StageChecker;
+import net.minecraft.inventory.InventoryCrafting;
+
+@Mixin (RecipeStage.class)
+public abstract class mixinRecipeStage {
+    @Inject(method = "isGoodForCrafting",remap = false,cancellable = true, at = { @At("HEAD") })
+    public void isGoodForCrafting(InventoryCrafting inv,CallbackInfoReturnable<Boolean> cir){
+        String tier = ((RecipeStage)(Object)this).getTier();
+        if(StageChecker.instance.stages.contains(tier)){
+            cir.setReturnValue(true);
+        }
+    }
+    
+}
